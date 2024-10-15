0ac61fc3f656d1060f56d6ddcbf6bd0b0f958157
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-10-15 20:40:18 +0100

    Create Theme enum to hold colour names as defined in the Assets folder

--- PeopleProject/Resources/Theme.swift
+++ PeopleProject/Resources/Theme.swift
@@ -5,4 +5,11 @@
 //  Created by Claire Roughan on 15/10/2024.
 //
 
-import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let pill = Color("pill")
+    static let textColor = Color("text")
+}
