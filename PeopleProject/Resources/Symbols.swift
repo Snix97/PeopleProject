68c6f41f866a72f74c87727b5523c10e029c176c
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-10-15 20:39:08 +0100

    Create enum to hold type safe SF symbol names to be used in the app

---
+++ PeopleProject/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  PeopleProject
+//
+//  Created by Claire Roughan on 15/10/2024.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ PeopleProject/Resources/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  PeopleProject
+//
+//  Created by Claire Roughan on 15/10/2024.
+//
+
+import Foundation
