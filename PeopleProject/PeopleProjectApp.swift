cd340e30c856333302952b791a802704ab443106
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-10-15 19:57:27 +0100

    Initial project setup

---
+++ PeopleProject/PeopleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleProjectApp.swift
+//  PeopleProject
+//
+//  Created by Claire Roughan on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeopleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PeopleProjectTests/PeopleProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleProjectTests.swift
+//  PeopleProjectTests
+//
+//  Created by Claire Roughan on 15/10/2024.
+//
+
+import Testing
+@testable import PeopleProject
+
+struct PeopleProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
