fac26716a65f6e8492305d6cd2f7f3802cd29d5c
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-11-08 22:52:29 +0000

    Create NewPerson model

---
+++ PeopleProject/Features/Create/Models/NewPerson.swift
@@ -0,0 +1,15 @@
+//
+//  NewPerson.swift
+//  PeopleProject
+//
+//  Created by Claire Roughan on 08/11/2024.
+//
+
+// MARK: - NewPerson
+struct NewPerson: Codable {
+    
+    // Defaults to Empty strings for person creation
+    var firstName: String = ""
+    var lastName: String = ""
+    var job: String = ""
+}
